024ba604f471f6897e44e8756a9fab4f77988ec6
MOminRifat <mominrifat8@gmail.com>
2026-02-19 10:29:32 +0600

    updates

--- portfolio/portfolio/api_handler.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from mangum import Mangum  # For serverless
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio/portfolio.settings")
-
-application = get_wsgi_application()
-handler = Mangum(application)  # Vercel entry point
