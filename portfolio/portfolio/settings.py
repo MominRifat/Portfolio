bfe06b99fcbe5afe647e15e7008aee33261fc893
MOminRifat <mominrifat8@gmail.com>
2026-02-19 09:12:23 +0600

    deploy

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(gyat9mgt$sh@e=5g6b*^fvi7peq7aavd70anq1tb$jss%ax#t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
